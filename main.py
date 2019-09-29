3bda3e3170e6882e548daf39861a916890101e83
-L0Lock- <myuline.annatar@gmail.com>
2019-09-29 18:08:40 +0200

    working drawing app

--- main.py
+++ main.py
@@ -1,138 +1,102 @@
-from tkinter import *
-import tkinter.font
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QAction, QShortcut, QFileDialog
+from PyQt5.QtGui import QIcon, QImage, QPainter, QPen
+from PyQt5.QtCore import Qt, QPoint
 
-# ---------- Define my class --------
+# source: https://www.youtube.com/watch?v=qEgyGyVA1ZQ
 
-class PaintApp:
 
-# ---------- Define class variables --------
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    drawing_tool = "pencil"
+        top = 400
+        left = 400
+        width = 800
+        height = 600
 
-    left_but = "up"
+        icon = "icons/icon.png"
 
-    x_pos, y_pos = None, None
+        self.setWindowTitle("ScreenPen drawing board")
+        self.setGeometry(top, left, width, height)
+        self.setWindowIcon(QIcon(icon))
 
-    x1_line_pt, y1_line_pt, x2_line_pt, y2_line_pt = None, None, None, None
+# ---------- sets image ----------
+        self.image = QImage(self.size(), QImage.Format_RGBA64)
+        self.image.fill(Qt.transparent)
+
+# ---------- init drawing state ----------
+        self.drawing = False
+        self.brushSize = 2
+        self.brushColor = Qt.black
+        self.lastPoint = QPoint()
+
+# ---------- Define Menus ----------
+    # mainmenu
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+        toolMenu = mainMenu.addMenu("Tool")
+        toolColor = mainMenu.addMenu("Color")
+    # smenu save
+        saveAction = QAction(QIcon("icons/save.png"), "Save", self)
+        saveAction.setShortcut("Ctrl+S")
+        fileMenu.addAction(saveAction)
+        saveAction.triggered.connect(self.saveFrame)
+    # smenu clear frame
+        clearFrameAction = QAction(QIcon("icons/clear.png"), "Clear Frame", self)
+        clearFrameAction.setShortcut("Ctrl+Del")
+        fileMenu.addAction(clearFrameAction)
+        clearFrameAction.triggered.connect(self.clearFrame)
+    # smenu Tool Pen
+        toolPenAction = QAction(QIcon("icons/toolPen.png"), "Pen", self)
+        # clearAction.setShortcut("Ctrl+Del")
+        toolMenu.addAction(toolPenAction)
 
 # ---------- Catch Mouse Down --------
 
-    def left_but_down(self, event=None):
-        self.left_but = "down"
-
-        self.x1_line_pt = event.x
-        self.y1_line_pt = event.y
-
-# ---------- Catch Mouse Up --------
-
-    def left_but_up(self, event=None):
-        self.left_but = "up"
-
-        self.x_pos = None
-        self.y_pos = None
-
-        self.x2_line_pt = event.x
-        self.y2_line_pt = event.y
-
-        if self.drawing_tool == "line":
-            self.line_draw(event)
-        elif self.drawing_tool == "arc":
-            self.arc_draw(event)
-        elif self.drawing_tool == "elipse":
-            self.elipse_draw(event)
-        elif self.drawing_tool == "rectangle":
-            self.rectangle_draw(event)
-        elif self.drawing_tool == "text":
-            self.text_draw(event)
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drawing = True
+            self.lastPoint = event.pos()
 
 # ---------- Catch Mouse Move --------
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.LeftButton) & self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, event.pos())
+            self.lastPoint = event.pos()
+            self.update()
 
-    def motion(self, event=None):
+# ---------- Catch Mouse Up --------
+    def mouseReleaseEvent(self, event):
+        if event.button == Qt.LeftButton:
+            self.drawing = False
 
-        if self.drawing_tool == "pencil":
-            self.pencil_draw(event)
+# ---------- Paint --------
+    def paintEvent(self, event):
+        canvasPainter = QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
 
-            
+# ---------- Save Action ----------
+    def saveFrame(self):
+        filePath,  _ = QFileDialog.getSaveFileName(self, "Save Image", "", "PNG(*.png);;JPEG(*.jpg *.jpeg);; ALL Files(*.*)")
+        if filePath == "":
+            return
+        self.image.save(filePath)
 
-# ---------- Draw Pencil --------
+# ---------- Clear Frame Action ----------
+    def clearFrame(self):
+        self.image.fill(Qt.white)
+        self.update()
 
-    def pencil_draw(self, event=None):
-
-        if self.left_but == "down":
-
-            if self.x_pos is not None and self.y_pos is not None:
-
-                event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y, smooth=TRUE, width=10)
-
-            self.x_pos = event.x
-            self.y_pos = event.y
-
-
-# ---------- Draw Line --------
-
-    def line_draw(self, event=None):
-
-        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt):
-
-            event.widget.create_line(self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt, smooth=TRUE, fill="green")
-
-# ---------- Draw Arc --------
-
-    def arc_draw(self, event=None):
-
-        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt):
-
-            coords = self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt
-
-            event.widget.create_arc(coords, start=0, extent=150, style=ARC)
-
-
-# ---------- Draw Elipse --------
-
-    def elipse_draw(self, event=None):
-
-        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt):
-
-            event.widget.create_oval(self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt, outline="black", width="2")
-
-# ---------- Draw Rectangle --------
-
-    def rectangle_draw(self, event=None):
-
-        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt):
-
-            event.widget.create_rectangle(self.x1_line_pt, self.y1_line_pt, self.x2_line_pt, self.y2_line_pt, outline="black", width="2")
-
-# ---------- Draw Text --------
-
-    def text_draw(self, event=None):
-
-        if None not in (self.x1_line_pt, self.y1_line_pt):
-
-            text_font = tkinter.font.Font(family='Helvetica', size=20, weight='bold', slant='italic')
-
-            event.widget.create_text(self.x1_line_pt, self.y1_line_pt, fill="black", font=text_font, text="wow")
-
-# ---------- Initialize --------
-
-    def __init__(self, root):
-        drawing_area = Canvas(root)
-
-        drawing_area.pack()
-
-        drawing_area.bind("<Motion>", self.motion)
-        drawing_area.bind("<ButtonPress-1>", self.left_but_down)
-        drawing_area.bind("<ButtonRelease-1>", self.left_but_up)
-
-root = Tk()
-root.title("ScreenPaint")
-
-
-
-paint_app = PaintApp(root)
-root.mainloop()
-
-
-
-
-
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Window = Window()
+    #''' Fenêtre transparente
+    Window.setStyleSheet("background:transparent;")
+    Window.setAttribute(Qt.WA_TranslucentBackground)
+    #Window.setWindowFlags(Qt.FramelessWindowHint)
+    #'''
+    Window.show()
+    app.exec()
